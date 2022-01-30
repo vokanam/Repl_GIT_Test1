4995934691312c5dcd28dcf4468347ee162dfd1d
Сергей Манаков <38924887+vokanam@users.noreply.github.com>
2022-01-30 10:28:14 +0000

    Ver 3.1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  std::cout << "Version history:\n\n";
 
   std::cout << "Ver 2\n";
 
   std::cout << "Ver 3\n";
+    std::cout << "Ver 3.1\n";
 }
